b5f9cb85109cd7023934b827636d9b69d8664c70
John Matthews <johnmatthews2634@gmail.com>
2018-03-02 15:10:34 -0800

    modify session actions

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -18,16 +18,16 @@ export const clearErrors = () => ({
 });
 
 export const signup = user => dispatch => (
-  APIUtil.signup(user).then(user => (
-    dispatch(receiveCurrentUser(user))
+  APIUtil.signup(user).then(user1 => (
+    dispatch(receiveCurrentUser(user1))
   ), err => (
     dispatch(receiveErrors(err.responseJSON))
   ))
 );
 
 export const login = user => dispatch => (
-  APIUtil.login(user).then(user => (
-    dispatch(receiveCurrentUser(user))
+  APIUtil.login(user).then(user1 => (
+    dispatch(receiveCurrentUser(user1))
   ), err => (
     dispatch(receiveErrors(err.responseJSON))
   ))
