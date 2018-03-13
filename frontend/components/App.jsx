7e8aa8154b05e959e39d1d07f878c0336b4d8fbb
John Matthews <johnmatthews2634@gmail.com>
2018-03-13 16:47:30 -0700

    more work on userauth

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import GreetingContainer from './greeting/greeting_container';
 
 const App = () => (
-  <div>
-    <h1>John</h1>
+  <div className='mainDiv'>
     <GreetingContainer />
   </div>
 );
--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -14,7 +14,6 @@ const sessionLinks = () => (
 
 const personalGreeting = (currentUser, logout) => (
 	<hgroup className="header-group" >
-
 	</hgroup>
 );
 
