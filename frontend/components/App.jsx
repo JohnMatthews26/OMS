3182610f428790927162036a1030ed48630f3e07
John Matthews <johnmatthews2634@gmail.com>
2018-03-15 11:46:36 -0700

    modify app.jsx

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import GreetingContainer from './greeting/greeting_container';
+import { Provider, Redirect } from 'react-redux';
+import {
+  Route,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
 
 const App = () => (
   <div className='mainDiv'>
