e4e5dd844943d124f415b0b9a12c3779d31f74c1
John Matthews <johnmatthews2634@gmail.com>
2018-03-15 11:52:50 -0700

    add session_form.jsx and session_form_container

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -7,6 +7,9 @@ import {
   Link,
   HashRouter
 } from 'react-router-dom';
+import {AuthRoute} from '../util/route_util';
+import {ProtectedRoute} from '../util/route_util';
+
 
 const App = () => (
   <div className='mainDiv'>
@@ -14,6 +17,10 @@ const App = () => (
       <h1>FindMyDog</h1>
       <GreetingContainer />
     </header>
+    <Switch>
+      <AuthRoute path="/login" component={SessionFormContainer} />
+      <AuthRoute path="/signup" component={SessionFormContainer} />
+    </Switch>
   </div>
 );
 
---
+++ frontend/components/session_form/session_form.jsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+
+class SessionForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDemoButton = this.handleDemoButton.bind(this);
+
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.match.path !== nextProps.match.path) {
+      this.props.clearErrors();
+    }
+    if (nextProps.loggedIn) {
+      this.props.history.push('/');
+    }
+  }
+
+  update(field) {
+    return e => this.setState({
+      [field]: e.currentTarget.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const user = this.state;
+    this.props.processForm({user});
+  }
+
+  signUpButtonText() {
+    if (this.props.formType === 'login') {
+      return "Login";
+    } else {
+      return "Sign up";
+    }
+  }
+
+
+  handleDemoButton(e) {
+    e.preventDefault();
+    const user = {username:'ILoveD0gs126', password:'password'};
+    this.props.processForm({user});
+  }
+
+  showDemoButton() {
+
+    if (this.props.formType === 'login') {
+      return (
+        <input type="submit" value='Demo' className="authsubmitbutton" onClick={this.handleDemoButton}/>
+      );
+    }
+  }
+
+
+  renderErrors() {
+    return(
+      <ul>
+        {this.props.errors.map((error, i) => (
+          <li key={`error-${i}`}>
+            {error}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div className='login-splash-div'>
+
+        <div className="login-form-container">
+          <form onSubmit={this.handleSubmit} className="login-form-box">
+
+            <br/>
+
+
+            <div className="login-form">
+              {this.renderErrors()}
+              <br/>
+              <label className="username_password_label">
+                <input type="text"
+                  value={this.state.username}
+                  onChange={this.update('username')}
+                  className="login-input"
+                  placeholder="Username"
+                />
+              </label>
+              <br/>
+              <label className="username_password_label">
+                <input type="password"
+                  value={this.state.password}
+                  onChange={this.update('password')}
+                  className="login-input"
+                  placeholder="Password"
+                />
+              </label>
+              <br/>
+              <input type="submit" value={this.signUpButtonText()} className="authsubmitbutton"/>
+              {this.showDemoButton()}
+            </div>
+          </form>
+        </div>
+
+
+      </div>
+    );
+  }
+}
+
+export default withRouter(SessionForm);
