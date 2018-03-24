e51b4bfea41b5042e3bae3dc599c5cfaaa79beec
John Matthews <johnmatthews2634@gmail.com>
2018-03-24 14:07:12 -0700

    add dog model

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,7 @@
 class Dog < ApplicationRecord
-  
+  validates :user_id, presence: true
+  validates :photo_url, presence: true
+  validates :breed, presence: true
+  validates :name, presence: true
+  belongs_to :user
 end
