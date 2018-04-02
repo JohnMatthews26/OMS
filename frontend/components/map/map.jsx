75a8e7f6751416f71a3d070a8af7786a6c2f24c5
John Matthews <johnmatthews2634@gmail.com>
2018-04-02 15:51:28 -0700

    work on map.jsx

---
+++ frontend/components/map/map.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {withRouter} from 'react-router';
+
+
+class Map extends Component {
+
+  render(){
+
+
+
+    return (
+
+      <div id='map-div'>
+
+      </div>
+    );
+  }
+}
+
+export default withRouter(Map);
