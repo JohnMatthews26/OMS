b09f41585c4c765e6aca9e297395a9f3f0675de9
John Matthews <johnmatthews2634@gmail.com>
2018-04-11 13:40:19 -0700

    add mapbox

--- frontend/components/map/map.jsx
+++ frontend/components/map/map.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {withRouter} from 'react-router';
-
+let mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
 class Map extends Component {
   constructor(props) {
@@ -17,7 +17,16 @@ class Map extends Component {
     return (
 
       <div id='map-div'>
+        <div id='map'>
 
+        </div>
+        <script>
+          mapboxgl.accessToken = 'pk.eyJ1Ijoiam9obnJvc2NvZTEyIiwiYSI6ImNqZnZqdHh6djBoY2EycW4xeWVxZmFwNHMifQ.Wc-DNL-Uf_07zf5yp1WByw';
+          let map = new mapboxgl.map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v10'
+          });
+        </script>
       </div>
     );
   }
