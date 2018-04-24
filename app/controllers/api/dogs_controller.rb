4f87faf5fefa75bb2010d26ebfa524bc0407ae65
John Matthews <johnmatthews2634@gmail.com>
2018-04-24 11:22:00 -0700

    add dogs controller methods

--- app/controllers/api/dogs_controller.rb
+++ app/controllers/api/dogs_controller.rb
@@ -1,2 +1,32 @@
 class Api::DogsController < ApplicationController
+
+  def create
+    @dog = Dog.new(dog_params)
+    @dog.user_id = current_user.id
+
+    if @dog.save
+      render "api/dogs/show"
+    else
+      render json: @dogs.errors.full_messages, status: 422
+    end
+
+  end
+
+  def index
+    
+  end
+
+  def destroy
+
+  end
+
+  def show
+
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:user_id, :photo_url, :breed, :name)
+  end
 end
